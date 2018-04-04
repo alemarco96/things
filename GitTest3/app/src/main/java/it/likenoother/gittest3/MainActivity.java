da9246c60e9ea0628e8306bc3d1d524f3d07a9a3
ER <er@likenoother.it>
2018-04-04 22:09:26 +0200

    test #3 commit #2
    Signed-off-by: ER <er@likenoother.it>

--- GitTest3/app/src/main/java/it/likenoother/gittest3/MainActivity.java
+++ GitTest3/app/src/main/java/it/likenoother/gittest3/MainActivity.java
@@ -2,12 +2,26 @@ package it.likenoother.gittest3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ToggleButton myTogglebutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTogglebutton=(ToggleButton)findViewById(R.id.myToggleButton);
+        myTogglebutton.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View v) {
+                                                  Toast.makeText(MainActivity.this,"prova",Toast.LENGTH_LONG).show();
+                                              }
+                                          }
+
+        );
+
     }
 }
