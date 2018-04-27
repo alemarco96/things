7105169d30360a5d0f326ffa22b9e3b939cc4b73
buffyeah <prandinip@gmail.com>
2018-04-27 16:38:40 +0200

    Inizializzazione

---
+++ DistanceAlert/app/src/main/java/group107/distancealert/Dwm.java
@@ -0,0 +1,28 @@
+package group107.distancealert;
+
+import java.util.List;
+
+//Classe utile per comunicazione con modulo DWM1001-Dev collegato tramite SPI alla Raspberry
+public class Dwm {
+
+    public Dwm(String busName) {
+
+    }
+
+    //metodi per la gestione SPI
+    public int getDistance(int id) {
+        int distance;
+
+        return distance;
+    }
+
+    public List<Integer> scanID(){
+
+    }
+
+    //ricevi pacchetto
+    private List<Integer> getPacket(int type, int[] value) {
+
+    }
+
+}
--- DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
+++ DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    Dwm dwm;
+    int id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dwm = new Dwm("SPI0.0");
+
     }
 }
