f7fdc328b1ce57f5610be683933ede47697cd1f4
buffyeah <prandinip@gmail.com>
2018-05-09 11:50:46 +0200

    TestUI

--- DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
+++ DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
@@ -2,22 +2,81 @@ package group107.distancealert;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//Nulla in questa classe è definitivo
+//Sono prove al momento.
 
 // classe view
 public class MainActivity extends Activity {
     DistanceController myController;
     int id;
-
-    //TODO (1) button collegato a AllertDialog il quale mostra gli id disponibili
-    //TODO (1) button che mostra misura su TextView
+    boolean alert;
+    int maxUserDistance;
+    int measuredDistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView distanceView = (TextView) findViewById(R.id.distance);
 
         //sceglie canale di comunicazione UART o SPI
-        myController = new DistanceController("SPI0.0");
+        final DistanceController myController = new DistanceController("SPI0.0");
+        alert = false;
+
+        /*TODO Bottone per la connessione ad un modulo specifico
+        final Button button = findViewById(R.id.connection);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+
+            }
+        });*/
+
+        // Visualizza dato di distanza
+        new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                        measuredDistance = myController.getDistance(id);
+                        distanceView.setText(R.string.distance + measuredDistance);
+
+                        //controllo distanza superata
+                        if (measuredDistance > maxUserDistance && !alert) {
+                            //Distanza superata e allarme non ancora attivo
+                            // quindi attivo allarme
+                            new Thread(new Runnable() {
+                                public void run() {
+                                    while (alert) {
+                                        // TODO allarme
+                                        Toast.makeText(MainActivity.this,R.string.alarm_toast,Toast.LENGTH_LONG);
+                                    }
+                                }
+                            }).start();
+                        }
+
+                }
+            }
+        }).start();
+
 
     }
+        /*TODO lifecycle activity
+        protected void onStart();
+
+        protected void onRestart();
+
+        protected void onResume();
+
+        protected void onPause();
+
+        protected void onStop();
+
+        protected void onDestroy();*/
+    }
+
 }
