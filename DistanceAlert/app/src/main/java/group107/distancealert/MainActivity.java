dae1c96213049f61cc5227f032d1a809086fc057
buffyeah <prandinip@gmail.com>
2018-05-09 12:19:55 +0200

    UItest

--- DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
+++ DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
@@ -2,10 +2,13 @@ package group107.distancealert;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 //Nulla in questa classe è definitivo
 //Sono prove al momento.
@@ -42,9 +45,9 @@ public class MainActivity extends Activity {
         new Thread(new Runnable() {
             public void run() {
                 while (true) {
+                    try {
                         measuredDistance = myController.getDistance(id);
                         distanceView.setText(R.string.distance + measuredDistance);
-
                         //controllo distanza superata
                         if (measuredDistance > maxUserDistance && !alert) {
                             //Distanza superata e allarme non ancora attivo
@@ -53,12 +56,15 @@ public class MainActivity extends Activity {
                                 public void run() {
                                     while (alert) {
                                         // TODO allarme
-                                        Toast.makeText(MainActivity.this,R.string.alarm_toast,Toast.LENGTH_LONG);
+                                        Toast.makeText(MainActivity.this, R.string.alarm_toast, Toast.LENGTH_LONG).show();
                                     }
                                 }
                             }).start();
                         }
 
+                    } catch (IOException e) {
+                        Log.i("getDistance(id)", "Errore nella ricezione della distanza");
+                    }
                 }
             }
         }).start();
