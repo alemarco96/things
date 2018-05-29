381a705f07c8facefa42f90ee1071cd2ef3a7201
ER <er@likenoother.it>
2018-05-29 17:29:30 +0200

    rimosso handler

--- DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
+++ DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
@@ -342,7 +342,7 @@ public class MainActivity extends Activity {
                     }
             );
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace();//TODO
         }
     }
 
