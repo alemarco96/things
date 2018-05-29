90a52975b0ca84230ba103b790068568cb841c3e
buffyeah <prandinip@gmail.com>
2018-05-29 11:59:47 +0200

    MainActivity RadioGroup da testare v2

--- DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
+++ DistanceAlert/app/src/main/java/group107/distancealert/MainActivity.java
@@ -2,7 +2,6 @@ package group107.distancealert;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,12 +19,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-//TODO possibile ottimizzazione codice riguardante aggiornamento lista IDs
-/*--> onTagHasConnected(List<DistanceController.Entry> tags) : tags è la lista dei tags appena
-connessi
---> onTagHasDisconnected(List<DistanceController.Entry> tags) : tags è la lista dei tags appena
-disconnessi
-*/
 public class MainActivity extends Activity {
     public static final String TAG = "107G";
 
@@ -116,10 +109,6 @@ public class MainActivity extends Activity {
                         public void onTagHasConnected(List<DistanceController.Entry> tags) {
                             Log.i(TAG,"MainActivity -> addAllTagListener ->" +
                                     " onTagHasConnected: tags.size() = " + tags.size());
-                            //almeno un Tag si è appena connesso, rigenerazione lista IDs
-                            //regenerateRagioGroup(listIDsGroup, idLayout, distanceView,
-                            //        connectedToId);
-
                             //aggiunga tags appena connessi
                             for(int i = 0; i < tags.size(); i++) {
                                 item.add(new RadioButton(getApplicationContext()));
@@ -157,10 +146,6 @@ public class MainActivity extends Activity {
                         public void onTagHasDisconnected(List<DistanceController.Entry> tags) {
                             Log.i(TAG,"MainActivity -> addAllTagListener" +
                                     " -> onTagHasDisconnected: tags.size() = " + tags.size());
-                            //almeno un Tag si è appena disconnesso, rigenerazione lista IDs
-                            //regenerateRagioGroup(listIDsGroup, idLayout, distanceView,
-                            //        connectedToId);
-
                             //rimozione tags appena disconnessi
                             for(int i = 0; i < tags.size(); i ++) {
                                 for(int j = 0; j < item.size(); j++) {
@@ -206,63 +191,6 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             //todo
         }
-    }
-
-    /**
-     * Rigenera la lista che gestisce gli IDs disponibili
-     * @param listIDsGroup RadioGroup da aggiornare
-     * @param idLayout LinearLayout contenente il RadioGroup da aggiornare
-     * @param distanceView La TextView dove viene visualizzato il dato di distanza rilevato
-     * @param connectedToId La TextView dove viene visualizzato l'ID del modulo a distanza
-     *                      "distanceView"
-     */
-    private void regenerateRagioGroup(final RadioGroup listIDsGroup, final LinearLayout idLayout,
-                                      final TextView distanceView, final TextView connectedToId){
-        Log.i(TAG, "MainActivity -> regenerateRadioGroup");
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Log.i(TAG,"MainActivity -> regenerateRadioGroup: running thread");
-                //ricezione IDs connessi
-                int[] ids = myController.getTagIDs();
-                //creazione di Array contentente i RadioGroups
-                final RadioButton[] item = new RadioButton[ids.length];
-                //pulizia RadioGroup ospitante i RadioButtons
-                listIDsGroup.removeAllViews();
-                //popolazione dell'array di RadioButtons
-                for(int i = 0; i < ids.length; i++) {
-                    Log.i(TAG, "MainActivity -> regenerateRadioGroup: ciclo for, i = " + i);
-                    item[i] = new RadioButton(getApplicationContext());
-                    final int singleId = ids[i];
-                    final String idText = Integer.toHexString(singleId);
-                    item[i].setText(idText);
-
-                    //Click specifico di ogni singolo RadioButton
-                    item[i].setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Log.i(TAG, "MainActivity -> regenerateRadioGroup:"
-                                    + " onClick " + idText);
-                            id = singleId;
-                            connectToSpecificListener(distanceView, connectedToId);
-                        }
-                    });
-                    //Controllo se il bottone era stato premuto in precedenza
-                    if(id != -1 && id == singleId) {
-                        Log.i(TAG, "MainActivity -> regenerateRadioGroup:" +
-                                " ciclo for, i = " + i + ", RadioButton toggled: (id = " + id +
-                                ") == (singleid = " + singleId + ")");
-                        item[i].toggle();
-                    }
-                    //Aggiunta del bottone in fondo alla lista
-                    listIDsGroup.addView(item[i], -1, ViewGroup.LayoutParams.WRAP_CONTENT);
-                }
-                //pulizia scorso idLayout, ospitante il RadioGroup
-                idLayout.removeAllViews();
-                //pubblicazione RadioGroup su idlayout
-                idLayout.addView(listIDsGroup);
-            }
-        });
     }
 
     /**
@@ -353,7 +281,8 @@ public class MainActivity extends Activity {
             myAlarm.start();
             pulsante.setEdgeTriggerType(Gpio.EDGE_RISING);
 
-            pulsante.registerGpioCallback(/*todo runtime exception create handler inside thread that has not called Looper.prepare() */
+            pulsante.registerGpioCallback(
+                    /*todo runtime exception create handler inside thread that has not called Looper.prepare() */
                     /*new Handler(),*/
                     new GpioCallback() {
                         @Override
