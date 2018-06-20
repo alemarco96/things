3be2aff990c564819892319f97f5809550d52a06
buffyeah <prandinip@gmail.com>
2018-06-20 15:06:27 +0200

    SleepHelper

--- DistanceAlert/app/src/main/java/group107/distancealert/SleepHelper.java
+++ DistanceAlert/app/src/main/java/group107/distancealert/SleepHelper.java
@@ -3,8 +3,6 @@ package group107.distancealert;
 import android.os.SystemClock;
 import android.util.Log;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * Classe ausiliaria che si occupa di gestire lo sleep dei thread
  */
@@ -19,7 +17,6 @@ public final class SleepHelper
      * Ferma il thread, tentando di porlo in sleep, per almeno un numero specifico di millisecondi
      * @param timeout Il numero di millisecondi
      */
-    @SuppressWarnings("EmptyCatchBlock")
     public static void sleepMillis(long timeout)
     {
         long startTime = SystemClock.uptimeMillis();
@@ -33,60 +30,5 @@ public final class SleepHelper
         long endTime = SystemClock.uptimeMillis();
 
         Log.v(TAG, "sleepMillis(" + timeout + " ms)=> tempo atteso: " + (endTime - startTime) + " ms.");
-    }
-
-    /**
-     * Ferma il thread, tentando di porlo in sleep, per almeno un numero specifico di microsecondi
-     * @param timeout Il numero di microsecondi
-     */
-    @SuppressWarnings("EmptyCatchBlock")
-    public static void sleepMicros(long timeout)
-    {
-        long startTime = SystemClock.elapsedRealtimeNanos() / 1000L;
-
-        /*
-        //se lo sleep fallisce, viene fatto busy-waiting
-        do {
-            try
-            {
-                TimeUnit.MICROSECONDS.sleep(timeout - ((SystemClock.elapsedRealtimeNanos() / 1000L) - startTime));
-            } catch (InterruptedException e) {
-                //ignora l'interrupt del thread, e logga l'eccezione lanciata
-                Log.e(TAG, "Sleep interrupted", e);
-            }
-        }while(((SystemClock.elapsedRealtimeNanos() / 1000L) - startTime) < timeout);
-        */
-
-        //per intervalli così brevi fare busy-waiting
-        while(((SystemClock.elapsedRealtimeNanos() / 1000L) - startTime) < timeout);
-
-        long endTime = SystemClock.elapsedRealtimeNanos() / 1000L;
-
-        Log.v(TAG, "sleepMicros(" + timeout + " us)=> tempo atteso: " + (endTime - startTime) + " us.");
-    }
-
-    /**
-     * Ferma il thread, tentando di porlo in sleep, per almeno un numero specifico di nanosecondi
-     * @param timeout Il numero di nanosecondi
-     */
-    @SuppressWarnings({"unused", "EmptyCatchBlock"})
-    public static void sleepNanos(long timeout)
-    {
-        long startTime = SystemClock.elapsedRealtimeNanos();
-
-        //se lo sleep fallisce, viene fatto busy-waiting
-        do {
-            try
-            {
-                TimeUnit.NANOSECONDS.sleep(timeout - (SystemClock.elapsedRealtimeNanos() - startTime));
-            } catch (InterruptedException e) {
-                //ignora l'interrupt del thread, e logga l'eccezione lanciata
-                Log.e(TAG, "Sleep interrupted", e);
-            }
-        }while((SystemClock.elapsedRealtimeNanos() - startTime) < timeout);
-
-        long endTime = SystemClock.elapsedRealtimeNanos();
-
-        Log.v(TAG, "sleepNanos(" + timeout + " ns)=> tempo atteso: " + (endTime - startTime) + " ns.");
     }
 }
